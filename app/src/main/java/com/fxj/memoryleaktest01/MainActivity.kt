77644dddec0eab5051b3cb474f83229af2032157
fuxianjin <fuxianjin-hj@huajiao.tv>
2019-08-09 19:19:56 +0800

    添加工程

---
+++ app/src/main/java/com/fxj/memoryleaktest01/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.fxj.memoryleaktest01
+
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.OrientationEventListener
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        val TAG:String=MainActivity::class.java.simpleName
+        var isLandscape:Boolean=false;
+    }
+
+    private var orientationEventListener:OrientationEventListener?=null
+    private lateinit var btnChange:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG,"**onCreate**")
+        setContentView(R.layout.activity_main)
+        btnChange=findViewById(R.id.btn_change_orientation)
+        btnChange.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                switchToLandscape()
+            }
+        })
+    }
+
+    fun switchToLandscape(){
+        Log.d(TAG,"**switchToLandscape**before isLandscape=${MainActivity.isLandscape}");
+        requestedOrientation=if(MainActivity.isLandscape) ActivityInfo.SCREEN_ORIENTATION_PORTRAIT else ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        MainActivity.isLandscape=!MainActivity.isLandscape
+        Log.d(TAG,"**switchToLandscape**after isLandscape=${isLandscape}");
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"**onStart**")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"**onRestart**")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"**onResume**")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Log.d(TAG,"**onConfigurationChanged**newConfig.orientation=${newConfig.orientation}")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"**onPause**")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"**onStop**")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"**onDestroy**")
+    }
+}
